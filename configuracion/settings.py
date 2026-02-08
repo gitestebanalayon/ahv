6b39c7c6421beb16c74b34d0f00727533cf33b38
gitestebanalayon <gitesteban0@gmail.com>
2026-02-08 00:32:46 -0400

    listo

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -93,12 +93,12 @@ CSRF_TRUSTED_ORIGINS = [
     'wss://*.onrender.com',  # Para WebSockets
 ]
 
-CSRF_COOKIE_DOMAIN = '.onrender.com'
-SESSION_COOKIE_DOMAIN = '.onrender.com'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# CSRF_COOKIE_DOMAIN = '.onrender.com'
+# SESSION_COOKIE_DOMAIN = '.onrender.com'
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
