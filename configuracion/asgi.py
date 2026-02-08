53e92f71503b385b9b1655c0899fd81e0a1c1ae6
gitestebanalayon <gitesteban0@gmail.com>
2026-02-08 01:11:34 -0400

    listo

--- configuracion/asgi.py
+++ configuracion/asgi.py
@@ -1,22 +1,23 @@
-# configuracion/asgi.py
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-import apps.sistema.websocket.routing  # Si tienes routing de WebSockets
+from channels.security.websocket import AllowedHostsOriginValidator
+import django
 
+# Configurar Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuracion.settings')
 django.setup()
 
-# Aplicación ASGI para HTTP y WebSockets
+# Importar después de configurar Django
+from apps.sistema.websocket.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                apps.sistema.websocket.routing.websocket_urlpatterns  # Tus rutas de WebSockets
+                websocket_urlpatterns
             )
         )
     ),
